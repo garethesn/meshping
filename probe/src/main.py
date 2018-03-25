56d68edbb352fab89f0976e381ba234dbef4ce3f
Gareth Eason <gareth.eason@signal2noise.ie>
2018-03-25 16:24:05 +0200

    Experiment with prometheus_client library

--- probe/src/main.py
+++ probe/src/main.py
@@ -9,23 +9,33 @@ Later:
 - Support configuration of additional nodes through the web api :9091/config
 '''
 
-# Definitions...
-hostname = 'localhost'
+# Configuration...
+hostname = ['localhost', '127.0.01']   # TODO: Make this dynamically configurable and reloadable
+probe_version = '0.1'
 webserver_port = 9091
 
 # Set up a webserver so our metrics can be scraped...
 from prometheus_client import start_http_server, Summary
 import random
 import time
+from ping import ping
 
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+REQUEST_TIME_PING_ARES = Summary('request_time_ping_ares_seconds',
+                                 'Time taken to test network connectivity to ares')
+
 
 # Decorate function with metric.
 @REQUEST_TIME.time()
 def process_request(t):
     """A dummy function that takes some time."""
     time.sleep(t)
+
+@REQUEST_TIME_PING_ARES.time()
+def ping_ares(t):
+    """A ping function that returns some statistics about network connectivity"""
+    ping('ares.signal2noise.ie')
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
